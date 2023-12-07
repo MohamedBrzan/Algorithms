374f446d49c4b1e90f040c04e47f8ff52000ba26
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-07 07:51:46 +0200

    Factorial of a number Algrotim

---
+++ FactorialOfANumber.js
@@ -0,0 +1,15 @@
+function FactorialOfANumber(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(FactorialOfANumber(1));
+console.log(FactorialOfANumber(2));
+console.log(FactorialOfANumber(3));
+console.log(FactorialOfANumber(4));
+console.log(FactorialOfANumber(5));
+console.log(FactorialOfANumber(6));
+console.log(FactorialOfANumber(7));
