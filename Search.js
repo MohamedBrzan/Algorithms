8943f574aa6021de2015fc91f485f07f4bbdf475
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-07 09:50:14 +0200

    Linear Search Algorithm

---
+++ Search.js
@@ -0,0 +1,11 @@
+//* Linear
+function Linear(n, t) {
+  for (let i = 0; i <= n.length; i++) {
+    if (t === n[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(Linear([1, 2, 3, 4, 5, 6], 5));
