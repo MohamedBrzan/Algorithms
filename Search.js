27f00771db04bfcdbb913e498ac85c126c177c49
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-07 10:12:29 +0200

    Binary Search Algorithm

--- Search.js
+++ Search.js
@@ -8,6 +8,30 @@ function Linear(n, t) {
   return -1;
 }
 
-console.log(Linear([1, 2, 3, 4, 5, 6], 5));
+// console.log(Linear([1, 2, 3, 4, 5, 6], 5));
 
 //* Big-O = O(n)
+
+//*********************************** */
+
+function Binary(n, t) {
+  let start = 0;
+  let end = n.length - 1;
+  while (end <= start) {
+    let mid = Math.floor((start + end) / 2);
+    if (t === n[mid]) return mid;
+
+    if (t < n[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+// console.log(Binary([1, 2, 3, 4, 5], 534));
+
+//* Big-O = O(logn)
+
+//*********************************** */
