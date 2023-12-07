2416cd4dfe1cc5376a8cace4c67e1efb4d6c6a8d
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-07 07:59:22 +0200

    Prime Number Algorithm

---
+++ PrimeNumber.js
@@ -0,0 +1,13 @@
+function isPrimeNumber(n) {
+  if (n < 2) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrimeNumber(1));
+console.log(isPrimeNumber(2));
+console.log(isPrimeNumber(3));
+console.log(isPrimeNumber(4));
+console.log(isPrimeNumber(5));
