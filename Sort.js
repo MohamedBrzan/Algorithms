b325d24cf5ad61e15eed6db490766ab5fede66e8
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-09 17:50:13 +0200

    Finished Quick Sort

--- Sort.js
+++ Sort.js
@@ -24,7 +24,7 @@ function InsertionSort(array) {
   for (let i = 1; i < array.length; i++) {
     let numberToInsert = array[i];
     let j = i - 1;
-    while (j > 0 && numberToInsert < array[j]) {
+    while (j >= 0 && numberToInsert < array[j]) {
       array[j + 1] = array[j];
       j--;
     }
@@ -34,10 +34,29 @@ function InsertionSort(array) {
   return array;
 }
 
-console.log(InsertionSort([-6, 20, 8, -2, 4]));
+// console.log(InsertionSort([8, 20, -2, 4, -6]));
 
 //* Big-O = O(n^2) Quadratic
 
 //*********************************************** */
 
+function QuickSort(array) {
+  let pivot = array[array.length - 1];
+  let left = [];
+  let right = [];
+  if (array.length < 2) return array;
 
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > pivot) {
+      right.push(array[i]);
+    } else if (array[i] < pivot) {
+      left.push(array[i]);
+    }
+  }
+  return [...QuickSort(left), pivot, ...QuickSort(right)];
+}
+
+console.log(QuickSort([-6, 20, 8, -2, 4]));
+
+//* Big-O = O(n^2) Quadratic
+//* Average-O = O(nlogn)
