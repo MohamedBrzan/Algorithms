27d9a7d541b0dd2991605ddd1bab2e950c04aef6
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-11 08:03:43 +0200

    Finished Climbng Staircase

--- ClimbingStaircase.js
+++ ClimbingStaircase.js
@@ -1,4 +1,15 @@
-function ClimbingStaircase  (){}
+function ClimbingStaircase(n) {
+  let numOfWays = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    numOfWays[i] = numOfWays[i - 1] + numOfWays[i - 2];
+  }
+  return numOfWays[n - 1];
+}
 
+console.log(ClimbingStaircase(1));
+console.log(ClimbingStaircase(2));
+console.log(ClimbingStaircase(3));
+console.log(ClimbingStaircase(4));
+console.log(ClimbingStaircase(5));
 
-console.log(ClimbingStaircase([]))
+//* Big-O  = O(n)
