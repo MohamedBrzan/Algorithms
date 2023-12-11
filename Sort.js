1c43fa8f15309a83dd6e8a26a7b5f4aa9d5dcadb
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-11 07:49:23 +0200

    Finished Merge Sort

--- Sort.js
+++ Sort.js
@@ -56,7 +56,33 @@ function QuickSort(array) {
   return [...QuickSort(left), pivot, ...QuickSort(right)];
 }
 
-console.log(QuickSort([-6, 20, 8, -2, 4]));
+// console.log(QuickSort([-6, 20, 8, -2, 4]));
 
 //* Big-O = O(n^2) Quadratic
 //* Average-O = O(nlogn)
+
+//*********************************************** */
+
+function MergeSort(arr) {
+  if (arr.length < 2) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+  return merge(MergeSort(leftArr), MergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+console.log(MergeSort([-6, 20, 8, -2, 4]));
+
+//* Big-O = O(nlogn)
